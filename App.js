e848f5c0b3860e60db5a7b182ef61ed430483eba
DevYeon <yeonkevin@icloud.com>
2018-07-05 08:35:09 +0900

    add Icons from expo

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Weather from './Weather';
 
 export default class App extends Component {
   state = {
-    isLoaded: false
+    isLoaded: true
   }
 
   render() {
     const { isLoaded } = this.state;
     return (
       <View style={styles.container}>
-        {isLoaded ? null :  <View style={styles.loading}>
-          <Text style={styles.loadingText}>Getting the Weather</Text>
-          </View>}
+        <StatusBar hidden={true}/>
+        {isLoaded ? (
+          <Weather /> 
+        ) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the Weather</Text>
+          </View>
+        )}
       </View>
     );
   }
